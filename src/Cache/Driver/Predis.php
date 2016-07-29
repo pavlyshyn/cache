180eb7bc3d4f6df4a318cca60ff65c2d8d57a0fe
Roma Pavlyshyn <pavlyshyn.roma@gmail.com>
2016-07-30 02:56:58 +0300

    Predis cache

--- src/Cache/Driver/Predis.php
+++ src/Cache/Driver/Predis.php
@@ -5,21 +5,42 @@ namespace Pavlyshyn\Cache\Driver;
 class Predis implements Pavlyshyn\Cache\Driver {
 
     private $client = null;
+    private $expire = 86400;
 
     public function __construct($client) {
         $this->client = $client;
     }
 
-    public function set($key, $value) {
-        return $this->client->set($key, $value);
+    public function __destruct() {
+        $this->client->disconnect();
+    }
+
+    public function set($key, $value, $expire = null) {
+        $this->client->set($key, $this->pack($value));
+        if (!$expire) {
+            $expire = $this->expire;
+        }
+        $cmd = $this->client->createCommand('EXPIRE');
+        $cmd->setArguments([$key, $expire]);
+        $this->client->executeCommand($cmd);
     }
 
     public function get($key) {
-        return $this->client->get($key);
+        return $this->unPack($this->client->get($key));
     }
 
     public function remove($key) {
-        return $this->client->del($key);
+        $cmd = $this->client->createCommand('DEL');
+        $cmd->setArguments([$key]);
+        $this->client->executeCommand($cmd);
+    }
+
+    protected function pack($value) {
+        return serialize($value);
+    }
+
+    protected function unPack($value) {
+        return unserialize($value);
     }
 
 }
