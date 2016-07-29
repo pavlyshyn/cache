58dfe8fa9547c06f81dc87bb4d6fb0bed02e5ea6
Roma Pavlyshyn <pavlyshyn.roma@gmail.com>
2016-07-30 02:51:22 +0300

    File cache

--- src/Cache/Driver/File.php
+++ src/Cache/Driver/File.php
@@ -5,25 +5,74 @@ namespace Pavlyshyn\Cache\Driver;
 class File implements Pavlyshyn\Cache\Driver {
 
     private $path;
+    private $expire = 86400;
 
     public function __construct($path = '') {
         $this->path = $path;
     }
 
-    public function set($key, $value) {
-        return file_put_contents($this->getFileName($key), $value);
+    public function set($key, $value, $expire = null) {
+        $cacheFile = $this->getFileName($key);
+        if (!$expire) {
+            $expire = $this->expire;
+        }
+
+        $item = $this->pack([
+            'value' => $value,
+            'expire' => (int) $expire + time(),
+        ]);
+
+        if (!file_put_contents($cacheFile, $item)) {
+            throw new CacheException('Error saving data with the key "' . $key . '" to the cache file.');
+        }
     }
 
     public function get($key) {
-        return file_get_contents($this->getFileName($key));
+        $path = $this->getFileName($key);
+        if (!file_exists($path)) {
+            return;
+        }
+        $data = $this->unPack(file_get_contents($path));
+        if (!$data || !$this->validateData($data) || $this->hasExpired($data['expire'])) {
+            return;
+        }
+        return $data['value'];
     }
 
     public function remove($key) {
-        
+        $path = $this->getFileName($key);
+        if (is_file($path)) {
+            return unlink($path);
+        }
+        return false;
     }
 
     private function getFileName($key) {
         return $this->path . '/' . $key;
+    }
+
+    protected function pack($value) {
+        return serialize($value);
+    }
+
+    protected function unPack($value) {
+        return unserialize($value);
+    }
+
+    protected function validateData($data) {
+        if (!is_array($data)) {
+            return false;
+        }
+        foreach (['value', 'expire'] as $missing) {
+            if (!array_key_exists($missing, $data)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected function hasExpired($expire) {
+        return (time() > $expire);
     }
 
 }
