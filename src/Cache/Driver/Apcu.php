6acd37e88f292a7e3a77add4d786f8d02f620412
Roma Pavlyshyn <pavlyshyn.roma@gmail.com>
2016-07-30 03:00:40 +0300

    init Apcu

---
+++ src/Cache/Driver/Apcu.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Pavlyshyn\Cache\Driver;
+
+class Apcu implements Pavlyshyn\Cache\Driver {
+
+    private $expire = 86400;
+
+    public function __construct() {
+        
+    }
+
+    public function set($key, $value) {
+        
+    }
+
+    public function get($key) {
+        
+    }
+
+    public function remove($key) {
+        
+    }
+
+}
