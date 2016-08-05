10a87088a1b5a78102e164d70b7269561234e1a1
Roma Pavlyshyn <pavlyshyn.roma@gmail.com>
2016-08-05 21:40:53 +0300

    Couchbase

--- src/Cache/Adapter/Couchbase.php
+++ src/Cache/Adapter/Couchbase.php
@@ -12,28 +12,35 @@ class Couchbase extends \Pavlyshyn\Cache\AbstractCache {
 
     private $client = null;
 
-    public function __construct($config = null) {
-        
+    public function __construct($host = 'localhost', $bucket = 'default') {
+        if (!extension_loaded('couchbase') || !class_exists('CouchbaseCluster')) {
+            $this->client = null;
+            return false;
+        }
+
+        $cluster = new \CouchbaseCluster('couchbase://' . $host);
+        $this->client = $cluster->openBucket($bucket);
     }
 
     public function set($key, $value, $expire = null) {
-        
+        $expireIn = ($expire) ? $expire : $this->expire;
+        $this->client->set($key, $value, $expireIn);
     }
 
     public function get($key) {
-        
+        return $this->client->get($key);
     }
 
     public function exists($key) {
-        
+        return (bool) $this->client->get($key);
     }
 
     public function remove($key) {
-        
+        $this->client->delete($key);
     }
 
     public function clear() {
-        
+        $this->client->flush();
     }
 
 }
