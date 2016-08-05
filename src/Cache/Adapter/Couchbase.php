01447502f9f07d81e03e12b92baea044af94469c
Roma Pavlyshyn <pavlyshyn.roma@gmail.com>
2016-08-05 21:32:00 +0300

    init Couchbase

---
+++ src/Cache/Adapter/Couchbase.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the pavlyshyn/cache package
+ * 
+ * @author Roman Pavlyshyn <roman@pavlyshyn.com>
+ */
+
+namespace Pavlyshyn\Cache\Adapter;
+
+class Couchbase extends \Pavlyshyn\Cache\AbstractCache {
+
+    private $client = null;
+
+    public function __construct($config = null) {
+        
+    }
+
+    public function set($key, $value, $expire = null) {
+        
+    }
+
+    public function get($key) {
+        
+    }
+
+    public function exists($key) {
+        
+    }
+
+    public function remove($key) {
+        
+    }
+
+    public function clear() {
+        
+    }
+
+}
